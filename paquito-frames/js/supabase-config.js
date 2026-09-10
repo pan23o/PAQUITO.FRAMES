68efc432eb77e8d34cbc1e2f325e46dd3ad653ad
pan23o <157624290+pan23o@users.noreply.github.com>
2026-09-10 15:23:13 +0200

    Add Supabase client configuration

---
+++ paquito-frames/js/supabase-config.js
@@ -0,0 +1,3 @@
+window.PF_SUPABASE_URL = 'https://djfjqecahztogacliavh.supabase.co';
+window.PF_SUPABASE_KEY = 'sb_publishable_tm8Tid_HSYtu6cxXQ3ddKA_RWN15BSB';
+window.PF_SUPABASE = window.supabase.createClient(window.PF_SUPABASE_URL, window.PF_SUPABASE_KEY);
